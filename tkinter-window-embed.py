be9f8cd0ad011c1d05c98aab5253d60f047f97e6
Eric Jaden Stautmeister <eric@stautmeister.ch>
2021-04-01 09:58:46 +0200

    added test button

--- tkinter-window-embed.py
+++ tkinter-window-embed.py
@@ -12,12 +12,15 @@ import numpy as np
 root = tkinter.Tk()
 root.wm_title("Embedding in Tk")
 
-fig = Figure(figsize=(5, 4), dpi=100)
-t = np.arange(0, 3, .01)
+# fig = Figure(figsize=(5, 4), dpi=100)
+# t = np.arange(50, 100, .01)
 # fig.add_subplot().plot(t, 2 * np.sin(2 * np.pi * t))
-ax = plt.axes()
-ax.arrow(0, 0, 0.5, 0.5, head_width=0.05, head_length=0.05, fc='k', ec='k')
-plt.show()
+
+fig = plt.figure()
+ax = fig.add_subplot().plot([3, 4],
+                            [5, 6])
+# ax = plt.axes()
+# ax.arrow(0, 0, 0.5, 0.5, head_width=0.05, head_length=0.05, fc='k', ec='k')
 
 canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
 canvas.draw()
@@ -33,11 +36,17 @@ canvas.mpl_connect("key_press_event", key_press_handler)
 
 button = tkinter.Button(master=root, text="Quit", command=root.quit)
 
+
+def randfunc():
+    pass
+randButton = tkinter.Button(master=root, text="Test Button", command=randfunc)
+
 # Packing order is important. Widgets are processed sequentially and if there
 # is no space left, because the window is too small, they are not displayed.
 # The canvas is rather flexible in its size, so we pack it last which makes
 # sure the UI controls are displayed as long as possible.
 button.pack(side=tkinter.BOTTOM)
+randButton.pack(side=tkinter.BOTTOM)
 toolbar.pack(side=tkinter.BOTTOM, fill=tkinter.X)
 canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
