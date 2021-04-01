c6f99f7799eb0f7a2a42959f585b33532b47260e
Eric Jaden Stautmeister <eric@stautmeister.ch>
2021-04-01 12:30:05 +0200

    -

--- tkinter-window-embed.py
+++ tkinter-window-embed.py
@@ -1,8 +1,7 @@
 import tkinter
 
 from matplotlib.backends.backend_tkagg import (
-    FigureCanvasTkAgg, NavigationToolbar2Tk)
-# Implement the default Matplotlib key bindings.
+    FigureCanvasTkAgg)
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
@@ -12,27 +11,12 @@ import numpy as np
 root = tkinter.Tk()
 root.wm_title("Embedding in Tk")
 
-# fig = Figure(figsize=(5, 4), dpi=100)
-# t = np.arange(50, 100, .01)
-# fig.add_subplot().plot(t, 2 * np.sin(2 * np.pi * t))
-
 fig = plt.figure()
-ax = fig.add_subplot().plot([3, 4],
-                            [5, 6])
-# ax = plt.axes()
-# ax.arrow(0, 0, 0.5, 0.5, head_width=0.05, head_length=0.05, fc='k', ec='k')
+ax = fig.add_subplot().plot([9, 5, 10, 20],
+                            [5, 6, 6, 7])
 
 canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
 canvas.draw()
-
-# pack_toolbar=False will make it easier to use a layout manager later on.
-toolbar = NavigationToolbar2Tk(canvas, root, pack_toolbar=False)
-toolbar.update()
-
-
-canvas.mpl_connect(
-    "key_press_event", lambda event: print(f"you pressed {event.key}"))
-canvas.mpl_connect("key_press_event", key_press_handler)
 
 button = tkinter.Button(master=root, text="Quit", command=root.quit)
 
@@ -41,13 +25,8 @@ def randfunc():
     pass
 randButton = tkinter.Button(master=root, text="Test Button", command=randfunc)
 
-# Packing order is important. Widgets are processed sequentially and if there
-# is no space left, because the window is too small, they are not displayed.
-# The canvas is rather flexible in its size, so we pack it last which makes
-# sure the UI controls are displayed as long as possible.
 button.pack(side=tkinter.BOTTOM)
 randButton.pack(side=tkinter.BOTTOM)
-toolbar.pack(side=tkinter.BOTTOM, fill=tkinter.X)
 canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
 tkinter.mainloop()
