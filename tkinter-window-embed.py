8c48f645b61197ae323ca6a6b8c17debee7ad40d
Eric Jaden Stautmeister <eric@stautmeister.ch>
2021-04-03 19:57:40 +0200

    color updates

--- tkinter-window-embed.py
+++ tkinter-window-embed.py
@@ -26,10 +26,20 @@ class PltCanvas:
         canv = FigureCanvasTkAgg(self.fig, master=Mainframe)
         canv.get_tk_widget().place(relx=0.35, rely=0.03, relheight=0.7, relwidth=0.632)
 
-
+# color variables
+bg = "#282c34"
+canvas_bg = "#"
+frame_bg = "#505255"
+button_bg = "#A9B6C9"
 # create main window
 root = tk.Tk()
-root.title("Embedding in Tk")
+root.wm_iconbitmap('wave-square-solid.ico')
+root.wm_title("Vector Program")
+# root.overrideredirect(True)
+root.geometry("+250+2500")
+root.lift()
+# root.wm_attributes("-topmost", True)
+# root.wm_attributes("-transparentcolor", "white")
 
 # get screen size and center the generalised window
 screen_Height = root.winfo_screenheight()
@@ -44,34 +54,34 @@ y_cord = (screen_Height - 80) / 2 - Height / 2
 root.geometry("%dx%d+%d+%d" % (Width, Height, x_cord, y_cord))
 
 # set mainframe
-Mainframe = tk.Frame(root, bg="gray")
+Mainframe = tk.Frame(root, bg=bg)
 Mainframe.place(relheight=1, relwidth=1)
 
 canvas = PltCanvas(Mainframe)
 
 # create button_frame and buttons
-button_frame = tk.Frame(master=Mainframe, bg="yellow")
+button_frame = tk.Frame(master=Mainframe, bg=frame_bg)
 button_frame.place(relx=0.0175, relwidth=0.315, rely=0.03, relheight=0.7)
 
-button_2d = tk.Button(master=button_frame, text="2D", font="Helvetica 20",
+button_2d = tk.Button(master=button_frame, text="2D", bg = button_bg,font="Helvetica 20",
                       command=lambda: canvas.create_graph2d([2, 4], [1, 7]))
-button_3d = tk.Button(master=button_frame, text="3D", font="Helvetica 20",
+button_3d = tk.Button(master=button_frame, text="3D", bg = button_bg, font="Helvetica 20",
                       command=lambda: canvas.create_graph3d([2, 4], [1, 7], [2, 5]))
-enter_button = tk.Button(master=button_frame, text="Enter", command=canvas.include_graph)
+enter_button = tk.Button(master=button_frame, text="Enter", bg = button_bg, command=canvas.include_graph)
 
 button_2d.place(relx=0.05, relwidth=0.425, rely=0.025, relheight=0.1)
 button_3d.place(relx=0.5125, relwidth=0.425, rely=0.025, relheight=0.1)
 enter_button.place(relx=0.05, relwidth=0.9, rely=0.9, relheight=0.05)
 
 # create settings_frame
-settings_frame = tk.Frame(master=Mainframe, bg="green")
+settings_frame = tk.Frame(master=Mainframe, bg=frame_bg)
 settings_frame.place(relx=0.0175, relwidth=0.315, rely=0.75, relheight=0.22)
 
-quit_button = tk.Button(master=settings_frame, text="Quit", command=root.quit)
+quit_button = tk.Button(master=settings_frame, text="Quit", bg = button_bg, command=root.quit)
 quit_button.place(rely=0.75, relheight=0.22, relx=0.025, relwidth=0.95)
 
 # create action_frame
-action_frame = tk.Frame(master=Mainframe, bg="blue")
+action_frame = tk.Frame(master=Mainframe, bg=frame_bg)
 action_frame.place(rely=0.75, relheight=0.22, relx=0.35, relwidth=0.632)
 
 tk.mainloop()
